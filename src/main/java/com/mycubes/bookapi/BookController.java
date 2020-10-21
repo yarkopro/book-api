23bd4598e1cd383b5f4a5228861d85418e09482a
Yaroslav Prokopovych <yarkopro@gmail.com>
2020-10-22 01:22:20 +0300

    Implement some controller methods

--- src/main/java/com/mycubes/bookapi/BookController.java
+++ src/main/java/com/mycubes/bookapi/BookController.java
@@ -1,10 +1,14 @@
 package com.mycubes.bookapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("book")
@@ -14,12 +18,29 @@ public class BookController {
 	BookRepository bookRepository;
 
 	@PostMapping()
-	public void createBook(Book book) {
-		bookRepository.save(book);
+	public ResponseEntity createBook(@RequestBody Book book) {
+		try {
+			bookRepository.save(book);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			System.err.println(e);
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 	@PatchMapping()
 	public void updateBook(Book book) {
 
+	}
+
+	@GetMapping
+	public List<Book> getBooks() {
+		return StreamSupport.stream(this.bookRepository.findAll().spliterator(), false)
+			.collect(Collectors.toList());
+	}
+
+	@GetMapping("/search")
+	public List<Book> searchBook(String queryString) {
+		return null;
 	}
 }
